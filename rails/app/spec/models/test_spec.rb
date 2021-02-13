37dca481d5ac01702b4c8642f004e06f1ee12511
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-02-14 04:31:52 +0900

    成功するテスト２パターン、失敗するテスト１パターン定義

--- rails/app/spec/models/test_spec.rb
+++ rails/app/spec/models/test_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Test, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "return_true method returns true" do
+    test = Test.new()
+    result = test.return_true()
+    expect(result).to be_truthy
+  end
+
+  it "return_false method returns false" do
+    test = Test.new()
+    result = test.return_false()
+    expect(result).to be_falsey
+  end
+  
+  it "fail test" do
+    test = Test.new()
+    result = test.return_false()
+    expect(result).to be_truthy
+  end
 end
