7751711a1e924e1507760d53b325878de13db984
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-02-22 01:11:37 +0900

    EC2インスタンスを生成するようにした

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -19,3 +19,34 @@ module "VPC" {
         }
     ]
 }
+
+module "RouteTable" {
+    source = "./RouteTable"
+    name = "deploy_test"
+    vpc_id = module.VPC.vpc_id
+    subnets = module.VPC.public_subnets
+    gateway_routes = [{
+        id = module.VPC.internet_gateway_id
+        cidr_block = "0.0.0.0/0"
+    }]
+}
+
+module "SecurityGroup" {
+    source = "./SecurityGroup"
+    name = "deploy_test"
+    vpc_id = module.VPC.vpc_id
+    subnets = module.VPC.public_subnets
+    gateway_routes = [{
+        id = module.VPC.internet_gateway_id
+        cidr_block = "0.0.0.0/0"
+    }]
+    enable_ssh = true
+    enable_http = true
+}
+
+module "EC2" {
+    source = "./EC2"
+    name = "deploy_test"
+    subnet = module.VPC.public_subnets[0]
+    security_groups = [module.SecurityGroup.id]
+}
